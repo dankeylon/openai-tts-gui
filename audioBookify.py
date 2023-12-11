61b532076d8125f4bce44e06cf90e2ad5245ca2f
Daniel Keylon <dankeylon@users.noreply.github.com>
2023-12-11 11:25:46 -0600

    Incremental update.  Iterating on the file management part of the classes.

--- audioBookify.py
+++ audioBookify.py
@@ -7,6 +7,8 @@ Created on Thu Nov  9 16:19:04 2023
 """
 import math
 import time
+
+from dotenv import load_dotenv
 from pathlib import Path
 from openai import OpenAI
 from pydub import AudioSegment
@@ -52,34 +54,51 @@ class Book():
                 
             chunks.append(book_text[start:end])
             
-            
         return chunks
 
     
 class TTS_API_Wrapper():
     
+    # Usage details from 
     max_requests_per_min = 50
+    token_size = 1000
+    cost_per_token = {"tts-1": 0.015, "tts-1-hd": 0.03}
     
     def __init__(self, book, out_path, model = "tts-1", voice = "onyx", overwrite_protect = True):
         
+        assert load_dotenv('.env')
+
         self.book = book
         self.out_path = out_path
         self.model = model
         self.voice = voice
         self.overwrite_protect = overwrite_protect
-    
-    def estimate_cost(self):
-        token_size = 1000 # characters
-        cost_per_token = {"tts-1": 0.015, "tts-1-hd": 0.03}
+
+    def estimate_cost(self):        
         
         total_cost = 0
         for chunk in self.book.chunks:
-            num_tokens = math.ceil(len(chunk) / token_size)
-            chunk_cost = num_tokens * cost_per_token[self.model]
+            num_tokens = math.ceil(len(chunk) / self.token_size)
+            chunk_cost = num_tokens * self.cost_per_token[self.model]
             total_cost += chunk_cost
             
         return total_cost
     
+    def create_paths_to_mp3s(self, len_of_chunks_list):
+
+        if len_chunks_list > 1:
+            paths = [Path(__file__).parent / 
+                    "mp3s" /
+                    (self.book.name + f"_{self.voice}_{self.model}_{idx}_of_{len_of_chunks_list}.mp3")
+                    for idx in range(0, len_of_chunks_list)]
+        elif len_chunks_list == 1: 
+            paths = [Path(__file__).parent / 
+                    "mp3s" /
+                    (self.book.name + f"_{self.voice}_{self.model}.mp3")
+                    for idx in range(0, len_of_chunks_list)]
+
+        return paths
+
     def request(self, chunk):
         
         return client.audio.speech.create(model=self.model,
@@ -89,31 +108,36 @@ class TTS_API_Wrapper():
     def spawn_requests(self):
         
         # Integrate joblib here, also find a way to rate limit the requests
-        if len(self.book.chunks) < 50:
+        if len(self.book.chunks) < self.max_requests_per_min:
             responses = [self.request(chunk) for chunk in self.book.chunks]
         
         return responses
     
-    def write_mp3s(self, audiobook_chunks):
+    def write_mp3s(self, audiobook_chunks, paths_to_mp3s, overwrite_protect = True):
         
-        pass
-    
+        assert len(audiobook_chunks) == len(paths_to_mp3s)
+
+        for idx, chunk in enumerate(audiobook_chunks):
+            if not (overwrite_protect and paths_to_mp3s[idx].exists()):
+                chunk.stream_to_file(paths_to_mp3s[idx])
+
+
     def join_mp3s(self, mp3_path_list):
+        # Use pydub to join the .mp3 chunks created by write_mp3s
+        mp3_list = [AudioSegment.from_mp3(file) for file in mp3_path_list]
+        output_file = mp3_list[0]
+        for file in mp3_list[1:]:
+            output_file = output_file + file
         
-        if len(book.chunks) > 1:
-            mp3_list = [AudioSegment.from_mp3(file) for file in mp3_path_list]
-            output_file = mp3_list[0]
-            for file in mp3_list[1:]:
-                output_file = output_file + file
-        
-        # This needs to be rewritten
-        output_file.export(Path(__file__).parent / "mp3s" / (name + f"_{self.voice}.mp3"), format="mp3")
+        # Export the full audiobook to a .mp3 file
+        output_file.export(Path(__file__).parent / "mp3s" / (name + f"_{self.voice}_{self.model}.mp3"), format="mp3")
     
     def create_audiobook(self):
-        
+        # Look into joining the mp3s before they are written to a file
         audiobook_chunks = self.spawn_requests()
-        mp3_path_list = self.write_mp3s(audiobook_chunks)
-        self.join_mp3s(mp3_path_list)
+        mp3_path_list = self.write_mp3s(audiobook_chunks, self.overwrite_protect)
+        if len(mp3_path_list) > 1:
+            self.join_mp3s(mp3_path_list)
 
     def create_sample(self):
 
